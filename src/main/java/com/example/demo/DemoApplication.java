76531392c73391964feb89777b1d7b86606e535e
Pravesh089 <71329807+Pravesh089@users.noreply.github.com>
2026-09-21 15:28:01 +0530

    Update comment for clarity on changes by Pravesh Malviya

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
         // changes done by feature/new branch
 
-        // changes done by Pravesh Malviya
+        // changes done by Pravesh Malviya (modification done)
 
         // changes done bby Pravesh089
     }
