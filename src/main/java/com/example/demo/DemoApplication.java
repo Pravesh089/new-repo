8de88e41e01c71251e396b3ad8c8faeb2160589f
Pravesh089 <71329807+Pravesh089@users.noreply.github.com>
2026-09-21 15:20:58 +0530

    Add comment about changes from another developer
    Added a comment indicating changes made by another developer.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        // changes done by some other dev in master branch
     }
 
 }
